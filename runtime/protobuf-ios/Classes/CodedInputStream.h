aa10614fe3f4a3a59f905bf469655bf9aad92762
simon <simon@MacBook-Pro.local>
2015-05-05 15:48:49 +0800

    support arc

--- runtime/protobuf-ios/Classes/CodedInputStream.h
+++ runtime/protobuf-ios/Classes/CodedInputStream.h
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import <Foundation/Foundation.h>
+
 @class PBExtensionRegistry;
 @class PBUnknownFieldSet_Builder;
 @protocol PBMessage_Builder;
